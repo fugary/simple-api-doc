0a27e21279283ff793d90110a07fd7e648ee5b19
gary.fu <xuebaiyouling@gmail.com>
2024-10-15 09:56:34 +0800

    处理Mysql执行失败问题

---
+++ simple-api-doc/src/main/resources/db/mysql/migration/V1_0_3__alter table text columns.sql
@@ -0,0 +1,3 @@
+alter table t_api_doc modify column doc_content longtext;
+alter table t_api_doc_schema modify column schema_content longtext;
+alter table t_api_project_info_detail modify column schema_content longtext;
