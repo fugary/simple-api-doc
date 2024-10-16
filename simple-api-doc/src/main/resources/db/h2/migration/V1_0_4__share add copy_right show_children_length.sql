be4959326574b6b1fa026bc98492255a131fcb2e
gary.fu <xuebaiyouling@gmail.com>
2024-10-16 18:11:57 +0800

    修改H2 flyway sql错误问题

--- simple-api-doc/src/main/resources/db/h2/migration/V1_0_4__share add copy_right show_children_length.sql
+++ simple-api-doc/src/main/resources/db/h2/migration/V1_0_4__share add copy_right show_children_length.sql
@@ -1,2 +1,2 @@
-ALTER TABLE t_api_project_share ADD COLUMN show_children_length bit(1);
+ALTER TABLE t_api_project_share ADD COLUMN show_children_length bit;
 ALTER TABLE t_api_project_share ADD COLUMN copy_right text;
