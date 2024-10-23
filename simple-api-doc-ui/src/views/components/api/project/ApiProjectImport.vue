de64583b6c1c8292001db1fe24f62e28a9ea9ab9
gary.fu <xuebaiyouling@gmail.com>
2024-10-23 15:51:44 +0800

    指定文件夹导入存在一定的问题，不再支持导入到文件夹

--- simple-api-doc-ui/src/views/components/api/project/ApiProjectImport.vue
+++ simple-api-doc-ui/src/views/components/api/project/ApiProjectImport.vue
@@ -140,7 +140,8 @@ const formOptions = computed(() => {
       tip: () => <div className="el-upload__tip">{$i18nBundle('api.msg.importFileLimit')}</div>
     }
   }, {
-    enabled: !!props.project?.id,
+    // enabled: !!props.project?.id,
+    enabled: false,
     labelKey: 'api.label.targetFolder',
     type: 'tree-select',
     prop: 'toFolder',
