d66d6ea32c61e6e2fd6a1779e144b2766bd4c68f
gary.fu <xuebaiyouling@gmail.com>
2025-11-24 14:11:50 +0800

    opt: api接口安全配置提取到新窗口中配置

--- simple-api-doc-ui/src/views/components/api/doc/comp/ApiDocSecurityRequirements.vue
+++ simple-api-doc-ui/src/views/components/api/doc/comp/ApiDocSecurityRequirements.vue
@@ -1,16 +1,10 @@
 <script setup lang="jsx">
-import { computed, ref } from 'vue'
-import { $i18nKey } from '@/messages'
+import { computed, useTemplateRef } from 'vue'
 import {
-  checkAndSaveDocInfoDetail,
-  getOauth2ConfigInfo,
-  newDocInfoDetail,
   securitySchemas2List
 } from '@/services/api/ApiDocEditService'
-import { defineFormOptions, defineTableButtons } from '@/components/utils'
-import { cloneDeep } from 'lodash-es'
 import { ElText } from 'element-plus'
-import { checkShowColumn } from '@/utils'
+import SecurityRequirementsWindow from '@/views/components/api/doc/comp/edit/SecurityRequirementsWindow.vue'
 
 defineProps({
   editable: {
@@ -23,12 +17,9 @@ const apiDocDetail = defineModel({
   type: Object,
   default: () => ({})
 })
-
-const securitySchemas = computed(() => {
-  return JSON.parse(apiDocDetail.value.projectInfoDetail?.securitySchemas?.schemaContent || '{}')
-})
 const securitySchemaList = computed(() => {
-  return securitySchemas2List(securitySchemas.value)
+  const securitySchemas = JSON.parse(apiDocDetail.value.projectInfoDetail?.securitySchemas?.schemaContent || '{}')
+  return securitySchemas2List(securitySchemas)
 })
 // doc支持的
 const securityRequirements = computed(() => {
@@ -40,91 +31,11 @@ const supportedSecurities = computed(() => {
   return securitySchemaList.value.filter(schema => supportedKeys.value.includes(schema.schemaName))
 })
 const hasSecurity = computed(() => !!supportedKeys.value?.length)
-const showSecurityConfigWindow = ref(false)
-const schemaEditModel = ref({
-  securityParams: []
-})
-const emit = defineEmits(['schemaUpdated', 'toEditSecuritySchemas'])
+defineEmits(['schemaUpdated', 'toEditSecuritySchemas'])
+const securityWinRef = useTemplateRef('securityWinRef')
 const toEditSecuritySchemas = () => {
-  const securityParams = cloneDeep(securitySchemaList.value)
-  let hasOauth2 = false
-  securityParams.forEach(securityParam => {
-    securityParam.needAuth = supportedKeys.value?.includes(securityParam.schemaName)
-    if (securityParam.schema?.type === 'oauth2') {
-      hasOauth2 = true
-      securityParam.scopes = securityRequirements.value.find(require => require[securityParam.schemaName])?.[securityParam.schemaName]
-      const oauth2Config = getOauth2ConfigInfo(securityParam.schema)
-      securityParam.scopesOptions = oauth2Config?.scopesOptions
-    }
-  })
-  schemaEditModel.value.securityParams = securityParams
-  schemaEditModel.value.hasOauth2 = hasOauth2
-  showSecurityConfigWindow.value = true
+  securityWinRef.value.toEditSecuritySchemas()
 }
-const saveSecuritySchemas = () => {
-  console.log('====================schemaEditModel', schemaEditModel.value)
-  const newSecurities = schemaEditModel.value.securityParams.filter(securityParam => !!securityParam.needAuth).map(securityParam => {
-    return {
-      [securityParam.schemaName]: securityParam.scopes || []
-    }
-  })
-  const securityRequirements = apiDocDetail.value?.securityRequirements ||
-      {
-        ...newDocInfoDetail(apiDocDetail.value),
-        bodyType: 'doc_security_requirements'
-      }
-  securityRequirements.schemaContent = JSON.stringify(newSecurities)
-  checkAndSaveDocInfoDetail(securityRequirements).then(() => emit('schemaUpdated'))
-  showSecurityConfigWindow.value = true
-}
-const formOptions = computed(() => {
-  return defineFormOptions([{
-    labelKey: 'api.label.needAuth',
-    prop: 'needAuth',
-    type: 'switch',
-    minWidth: '50px'
-  }, {
-    labelKey: 'common.label.name',
-    prop: 'schemaName',
-    disabled: true,
-    minWidth: '100px'
-  }, {
-    label: 'Scopes',
-    prop: 'scopes',
-    minWidth: '150px',
-    type: 'select',
-    attrs: {
-      multiple: true
-    },
-    enabled: schemaEditModel.value.hasOauth2,
-    dynamicOption (data) {
-      console.log('===============data', data)
-      return {
-        enabled: data.schema?.type === 'oauth2',
-        children: data.scopesOptions || []
-      }
-    }
-  }, {
-    labelKey: 'common.label.description',
-    formatter (data) {
-      return <ElText type="info">{data}</ElText>
-    },
-    enabled: checkShowColumn(schemaEditModel.value?.securityParams, 'description'),
-    prop: 'schema.description',
-    type: 'common-form-label',
-    minWidth: '250px'
-  }])
-})
-const buttons = computed(() => {
-  return defineTableButtons([{
-    type: 'success',
-    label: $i18nKey('common.label.commonEdit', 'api.label.authDetails'),
-    click () {
-      emit('toEditSecuritySchemas')
-      showSecurityConfigWindow.value = false
-    }
-  }])
-})
 </script>
 
 <template>
@@ -183,22 +94,13 @@ const buttons = computed(() => {
         {{ $t('common.msg.noData') }}
       </el-text>
     </el-container>
-    <common-window
-      v-model="showSecurityConfigWindow"
-      width="1000px"
-      :title="$i18nKey('common.label.commonEdit', 'api.label.authorization')"
-      :ok-click="saveSecuritySchemas"
-      :buttons="buttons"
-    >
-      <el-container class="flex-column">
-        <common-table-form
-          :model="schemaEditModel"
-          :form-options="formOptions"
-          data-list-key="securityParams"
-          :show-operation="false"
-        />
-      </el-container>
-    </common-window>
+    <security-requirements-window
+      ref="securityWinRef"
+      :project-info-detail="apiDocDetail.projectInfoDetail"
+      :api-doc-detail="apiDocDetail"
+      @schema-updated="$emit('schemaUpdated')"
+      @to-edit-security-schemas="$emit('toEditSecuritySchemas')"
+    />
   </el-container>
 </template>
 
---
+++ simple-api-doc-ui/src/views/components/api/doc/comp/edit/SecurityRequirementsWindow.vue
@@ -0,0 +1,164 @@
+<script setup lang="jsx">
+import { computed, ref } from 'vue'
+import { cloneDeep } from 'lodash-es'
+import { $i18nKey } from '@/messages'
+import {
+  checkAndSaveDocInfoDetail,
+  getOauth2ConfigInfo,
+  newDocInfoDetail,
+  securitySchemas2List
+} from '@/services/api/ApiDocEditService'
+import { defineFormOptions, defineTableButtons } from '@/components/utils'
+import { checkShowColumn } from '@/utils'
+import { ElText } from 'element-plus'
+
+const props = defineProps({
+  apiDocDetail: {
+    type: Object,
+    default: null
+  },
+  projectInfoDetail: {
+    type: Object,
+    required: true
+  },
+  docSecurity: {
+    type: Boolean,
+    default: true
+  }
+})
+const securitySchemaList = computed(() => {
+  const securitySchemas = JSON.parse(props.projectInfoDetail?.securitySchemas?.schemaContent || '{}')
+  return securitySchemas2List(securitySchemas)
+})
+// 支持的
+const getSecurityRequirements = (create) => {
+  let securityRequirements = props.docSecurity ? props.apiDocDetail?.securityRequirements : props.projectInfoDetail?.securityRequirements
+  if (!securityRequirements && create) {
+    if (props.docSecurity) {
+      securityRequirements = {
+        ...newDocInfoDetail(props.apiDocDetail),
+        bodyType: 'doc_security_requirements'
+      }
+    } else {
+      securityRequirements = {
+        infoId: props.projectInfoDetail?.id,
+        projectId: props.projectInfoDetail?.projectId,
+        bodyType: 'security_requirements'
+      }
+    }
+  }
+  return securityRequirements
+}
+const securityRequirements = computed(() => {
+  const securityRequirements = getSecurityRequirements()
+  return JSON.parse(securityRequirements?.schemaContent || '[]')
+})
+const supportedKeys = computed(() => securityRequirements.value.flatMap(config => Object.keys(config)))
+const showSecurityConfigWindow = ref(false)
+const schemaEditModel = ref({
+  securityParams: []
+})
+const emit = defineEmits(['schemaUpdated', 'toEditSecuritySchemas'])
+const toEditSecuritySchemas = () => {
+  const securityParams = cloneDeep(securitySchemaList.value)
+  let hasOauth2 = false
+  securityParams.forEach(securityParam => {
+    securityParam.needAuth = supportedKeys.value?.includes(securityParam.schemaName)
+    if (securityParam.schema?.type === 'oauth2') {
+      hasOauth2 = true
+      securityParam.scopes = securityRequirements.value.find(require => require[securityParam.schemaName])?.[securityParam.schemaName]
+      const oauth2Config = getOauth2ConfigInfo(securityParam.schema)
+      securityParam.scopesOptions = oauth2Config?.scopesOptions
+    }
+  })
+  schemaEditModel.value.securityParams = securityParams
+  schemaEditModel.value.hasOauth2 = hasOauth2
+  showSecurityConfigWindow.value = true
+}
+const saveSecuritySchemas = () => {
+  console.log('====================schemaEditModel', schemaEditModel.value)
+  const newSecurities = schemaEditModel.value.securityParams.filter(securityParam => !!securityParam.needAuth).map(securityParam => {
+    return {
+      [securityParam.schemaName]: securityParam.scopes || []
+    }
+  })
+  const securityRequirements = getSecurityRequirements(true)
+  securityRequirements.schemaContent = JSON.stringify(newSecurities)
+  checkAndSaveDocInfoDetail(securityRequirements).then(() => emit('schemaUpdated'))
+  showSecurityConfigWindow.value = true
+}
+const formOptions = computed(() => {
+  return defineFormOptions([{
+    labelKey: 'api.label.needAuth',
+    prop: 'needAuth',
+    type: 'switch',
+    minWidth: '50px'
+  }, {
+    labelKey: 'common.label.name',
+    prop: 'schemaName',
+    disabled: true,
+    minWidth: '100px'
+  }, {
+    label: 'Scopes',
+    prop: 'scopes',
+    minWidth: '150px',
+    type: 'select',
+    attrs: {
+      multiple: true
+    },
+    enabled: schemaEditModel.value.hasOauth2,
+    dynamicOption (data) {
+      console.log('===============data', data)
+      return {
+        enabled: data.schema?.type === 'oauth2',
+        children: data.scopesOptions || []
+      }
+    }
+  }, {
+    labelKey: 'common.label.description',
+    formatter (data) {
+      return <ElText type="info">{data}</ElText>
+    },
+    enabled: checkShowColumn(schemaEditModel.value?.securityParams, 'description'),
+    prop: 'schema.description',
+    type: 'common-form-label',
+    minWidth: '250px'
+  }])
+})
+const buttons = computed(() => {
+  return defineTableButtons([{
+    type: 'success',
+    label: $i18nKey('common.label.commonEdit', 'api.label.authDetails'),
+    click () {
+      emit('toEditSecuritySchemas')
+      showSecurityConfigWindow.value = false
+    }
+  }])
+})
+defineExpose({
+  toEditSecuritySchemas
+})
+</script>
+
+<template>
+  <common-window
+    v-model="showSecurityConfigWindow"
+    width="1000px"
+    :title="$i18nKey('common.label.commonEdit', 'api.label.authorization')"
+    :ok-click="saveSecuritySchemas"
+    :buttons="buttons"
+  >
+    <el-container class="flex-column">
+      <common-table-form
+        :model="schemaEditModel"
+        :form-options="formOptions"
+        data-list-key="securityParams"
+        :show-operation="false"
+      />
+    </el-container>
+  </common-window>
+</template>
+
+<style scoped>
+
+</style>
