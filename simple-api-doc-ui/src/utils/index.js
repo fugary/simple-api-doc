b19cfc2699aa3006e4d3c219d0f30a98bd9fa1cd
gary <xuebaiyouling@gmail.com>
2026-01-31 18:24:37 +0800

    bug: 修复部分情况下tab显示异常问题

--- simple-api-doc-ui/src/utils/index.js
+++ simple-api-doc-ui/src/utils/index.js
@@ -483,7 +483,7 @@ export const checkShowColumn = (dataList, field) => {
 export const calcAffixOffset = (fix = 10) => {
   let initValue = 60 + fix
   useGlobalConfigStore().layoutMode === GlobalLayoutMode.TOP && useGlobalConfigStore().isShowBreadcrumb && (initValue += 40)
-  useTabsViewStore().isTabMode && (initValue += 46)
+  useTabsViewStore().isTabMode && (initValue += 48)
   return initValue
 }
 
