e795a6286b23fc426a1b4cfe2f7e02125aa7ff47
gary <xuebaiyouling@gmail.com>
2026-02-08 21:14:48 +0800

    bug: 修复分享页切换主题时闪烁问题

--- simple-api-doc-ui/src/services/api/ApiFolderService.js
+++ simple-api-doc-ui/src/services/api/ApiFolderService.js
@@ -503,6 +503,7 @@ const handleShareThemeChange = (shareDarkTheme, event, callback) => {
       {
         duration: 400,
         easing: 'ease-in',
+        fill: 'forwards',
         pseudoElement: isDark ? '::view-transition-new(root)' : '::view-transition-old(root)'
       }
     )
