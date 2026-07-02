184538f6d42a0aedc79ba4b690d557e740e46484
gary.fu <xuebaiyouling@gmail.com>
2026-07-02 15:30:54 +0800

    opt: 增大默认的jvm对内存大小

--- simple-api-doc/src/main/resources/assemblies/bin/start.bat
+++ simple-api-doc/src/main/resources/assemblies/bin/start.bat
@@ -2,7 +2,7 @@
 cd ..
 title @project.name@
 echo Starting the @project.name@ ...
-set JAVA_OPTS=-Xms256M -Xmx512M
+set JAVA_OPTS=-Xms512M -Xmx1024M
 set DB_H2_CONSOLE=true
 set DB_DATA_DIR=./data
 java %JAVA_OPTS% -Dfile.encoding=UTF-8 -jar @project.build.finalName@.jar
--- simple-api-doc/src/main/resources/assemblies/bin/start_mysql.bat
+++ simple-api-doc/src/main/resources/assemblies/bin/start_mysql.bat
@@ -1,7 +1,7 @@
 cd ..
 title @project.name@
 echo Starting the @project.name@ ...
-set JAVA_OPTS=-Xms256M -Xmx512M
+set JAVA_OPTS=-Xms512M -Xmx1024M
 set DB_TYPE=mysql
 set DB_MYSQL_SERVER=localhost
 set DB_MYSQL_PORT=3306
