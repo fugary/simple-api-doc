2e1ccf0abfd110ff20e522d7590df7d8a551d360
gary.fu <xuebaiyouling@gmail.com>
2026-09-02 14:40:03 +0800

    opt: 优化md等文档滚动时目录没有随着滚动显示问题

--- simple-api-doc-ui/src/views/components/api/doc/comp/MarkdownDocCatalog.vue
+++ simple-api-doc-ui/src/views/components/api/doc/comp/MarkdownDocCatalog.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import { MdCatalog } from 'md-editor-v3'
 import CommonIcon from '@/components/common-icon/index.vue'
 
@@ -25,6 +26,23 @@ const isCollapsed = defineModel('collapsed', {
   type: Boolean,
   default: false
 })
+
+const scrollbarRef = ref()
+
+const handleActive = (tocItem, ele) => {
+  if (isCollapsed.value || !ele || !scrollbarRef.value?.wrapRef) return
+  const wrap = scrollbarRef.value.wrapRef
+  const wrapRect = wrap.getBoundingClientRect()
+  const eleRect = ele.getBoundingClientRect()
+
+  if (eleRect.top < wrapRect.top + 20) {
+    const diff = eleRect.top - wrapRect.top - 20
+    scrollbarRef.value.setScrollTop(Math.max(0, wrap.scrollTop + diff))
+  } else if (eleRect.bottom > wrapRect.bottom - 20) {
+    const diff = eleRect.bottom - wrapRect.bottom + 20
+    scrollbarRef.value.setScrollTop(wrap.scrollTop + diff)
+  }
+}
 
 const toggleCollapse = () => {
   isCollapsed.value = !isCollapsed.value
@@ -78,13 +96,17 @@ const toggleCollapse = () => {
           />
         </div>
       </div>
-      <el-scrollbar class="md-doc-catalog-scrollbar">
+      <el-scrollbar
+        ref="scrollbarRef"
+        class="md-doc-catalog-scrollbar"
+      >
         <md-catalog
           class="md-catalog"
           :editor-id="editorId"
           :theme="theme"
           :scroll-element="scrollElement"
           :scroll-element-offset-top="scrollElementOffsetTop"
+          :on-active="handleActive"
         />
       </el-scrollbar>
     </div>
